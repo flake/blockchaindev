0e8664467c85382f8dfdbf2e11267ad556376af8
Rajasekhar <raj.cynosure@gmail.com>
2021-07-03 22:09:17 +0530

    p2p server

--- app/index.js
+++ app/index.js
@@ -2,22 +2,28 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const Blockchain = require('../blockchain')
+const P2pServer = require('./p2p-server')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
-const bc = new Blockchain();
+const bc = new Blockchain()
+const p2pServer = new P2pServer(bc)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/api/blocks', (req, res) => {
-    res.json(bc.chain);
+    res.json(bc.chain)
 })
 
 app.post('/api/mine', (req, res) => {
-    const block = bc.addBlock(req.body.data);
-    console.log(`New block added ${block.toString()}`);
-    res.redirect('/api/blocks');
+    const block = bc.addBlock(req.body.data)
+    console.log(`New block added ${block.toString()}`)
+
+    p2pServer.syncChains()
+
+    res.redirect('/api/blocks')
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen()
