c5ec8d4b8439ea20c13a439cbbe93cb37fa28024
Rajasekhar <raj.cynosure@gmail.com>
2021-07-04 00:53:54 +0530

    wallet n transactions

--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,15 @@
-const Blockchain = require('./blockchain')
+const Wallet = require('./wallet')
 
-const bc = new Blockchain()
+const wallet = new Wallet()
+console.log(wallet.toString())
 
-for (let i = 0; i < 10; i++) {
-    console.log(bc.addBlock(`foo ${i}`).toString())
-}
+
+
+
+// const Blockchain = require('./blockchain')
+
+// const bc = new Blockchain()
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(bc.addBlock(`foo ${i}`).toString())
+// }
