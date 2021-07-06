744c94d1f97e6df1c9fdc5c534b491aac0283d58
Rajasekhar <raj.cynosure@gmail.com>
2021-07-06 20:03:04 +0530

    collect transactions in a pool

--- app/index.js
+++ app/index.js
@@ -3,17 +3,29 @@ const bodyParser = require('body-parser')
 
 const Blockchain = require('../blockchain')
 const P2pServer = require('./p2p-server')
+const Wallet = require('../wallet')
+const TransactionPool = require('../wallet/transaction-pool')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
-const p2pServer = new P2pServer(bc)
+const wallet = new Wallet()
+const tp = new TransactionPool()
+const p2pServer = new P2pServer(bc, tp)
 
 app.use(bodyParser.json())
 
 app.get('/api/blocks', (req, res) => {
     res.json(bc.chain)
+})
+
+app.get('/api/transactions', (req, res) => {
+    res.json(tp.transactions)
+})
+
+app.get('/api/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey })
 })
 
 app.post('/api/mine', (req, res) => {
@@ -25,5 +37,14 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks')
 })
 
-app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+app.post('/api/transact', (req, res) => {
+    const { recipient, amount } = req.body
+    const transaction = wallet.createTransaction(recipient, amount, tp)
+
+    p2pServer.broadcastTransaction(transaction)
+
+    res.redirect('/api/transactions')
+})
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
 p2pServer.listen()
