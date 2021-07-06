d9a89400568ec3fbba8546965b4e9d9cc5086a5e
Rajasekhar <raj.cynosure@gmail.com>
2021-07-06 23:31:46 +0530

    mine transactions in a block

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ const Blockchain = require('../blockchain')
 const P2pServer = require('./p2p-server')
 const Wallet = require('../wallet')
 const TransactionPool = require('../wallet/transaction-pool')
+const Miner = require('./miner')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
@@ -13,6 +14,7 @@ const bc = new Blockchain()
 const wallet = new Wallet()
 const tp = new TransactionPool()
 const p2pServer = new P2pServer(bc, tp)
+const miner = new Miner(bc, tp, wallet, p2pServer)
 
 app.use(bodyParser.json())
 
@@ -28,6 +30,12 @@ app.get('/api/public-key', (req, res) => {
     res.json({ publicKey: wallet.publicKey })
 })
 
+app.get('/api/mine-transactions', (req, res) => {
+    const block = miner.mine()
+    console.log(`New block added: ${block.toString()}`)
+    res.redirect('/api/blocks')
+})
+
 app.post('/api/mine', (req, res) => {
     const block = bc.addBlock(req.body.data)
     console.log(`New block added ${block.toString()}`)
@@ -39,7 +47,7 @@ app.post('/api/mine', (req, res) => {
 
 app.post('/api/transact', (req, res) => {
     const { recipient, amount } = req.body
-    const transaction = wallet.createTransaction(recipient, amount, tp)
+    const transaction = wallet.createTransaction(recipient, amount, bc, tp)
 
     p2pServer.broadcastTransaction(transaction)
 
